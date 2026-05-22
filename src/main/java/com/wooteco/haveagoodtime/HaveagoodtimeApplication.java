48fbbd5fa8a86ac7a71982cd0f569c1a1592a82b
ghkdtjfxkd <syt1128@gmail.com>
2026-05-22 14:32:17 +0900

    setup project

---
+++ src/main/java/com/wooteco/haveagoodtime/HaveagoodtimeApplication.java
@@ -0,0 +1,13 @@
+package com.wooteco.haveagoodtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveagoodtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaveagoodtimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wooteco/haveagoodtime/HaveagoodtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooteco.haveagoodtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaveagoodtimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
